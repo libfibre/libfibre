1a6090be8874f3a69cc1e480f0a4d23ad54daf8f
Geoff Thorpe <geoff@openssl.org>
2015-12-23 11:38:14 -0600

    build: make debug-gcc consistent with default-gcc
    Signed-off-by: Geoff Thorpe <geoff@openssl.org>

--- targets/debug-gcc.mk
+++ targets/debug-gcc.mk
@@ -5,7 +5,9 @@ CFLAGS += -DFIBRE_RUNTIME_CHECK
 AR := ar
 ARFLAGS := rcs
 
-FIBRE_ARCH ?= ucontext
+ifndef FIBRE_ARCH
+$(error FIBRE_ARCH needs to be defined; e.g. ucontext, setjmp, x86)
+endif
 
 # Slurp in the standard routines for parsing Makefile content and
 # generating dependencies
